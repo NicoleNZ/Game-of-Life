8d977c6e1607b76ac9d16066c1aa7b3b68ba7861
Nicole <nicole.barnett.nz@gmail.com>
2021-04-17 20:00:19 +1200

    I have been trying to get this to work for hours - I actually need to try it a different waygit add *

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,7 +1,31 @@
 //Create 2D array
 
-const gameOfLifeStructureArray = [[1,2,3],[4,5,6],[7,8,9]]; //plan to expand this to a larger array structure once I have the functionality working
-const gameOfLifeObjectArray = [];
+const gameOfLifeStructureArray = [
+    [1,2,3],[4,5,6],[7,8,9]];
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+//     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+
+// ];
+
+
 
 function getRandomInt(max) {
     let outcome = Math.floor(Math.random() * max);
@@ -15,6 +39,7 @@ function getRandomInt(max) {
 
 //Create FOR loop, to loop through each element in the array 
 //Create a new object with each iteration, that holds the cell location and randomly assigns dead or alive state.
+const gameOfLifeObjectArray = [];
 
 for (let i = 0; i < gameOfLifeStructureArray.length; i ++) {
     for (let j = 0; j < gameOfLifeStructureArray[i].length; j ++) {
@@ -27,38 +52,30 @@ for (let i = 0; i < gameOfLifeStructureArray.length; i ++) {
 console.log(gameOfLifeObjectArray);
 
 //Create a list of all the possible neighbours a cell might have
-// i - 1 && j - 1
-// i - 1 && j + 1
-// i - 1 && j
-// i + 1 && j - 1
-// i + 1 && j + 1
-// i + 1 && j
-// i && j - 1
-// i && j + 1
+
+
 
 //Create function to count how many alive neighbours a cell has.
+const findNeighbours = [[0, 1], [0, -1], [1, -1], [-1, 1], [1, 1], [-1, -1], [1, 0], [-1, 0]];
 
 let numberOfAliveNeighbours = 0;
 
-for (let i = 0; i < gameOfLifeObjectArray.length; i ++) {
-    if (gameOfLifeObjectArray[i].state === true) {
-        numberOfAliveNeighbours = numberOfAliveNeighbours + 1;
-    };
-};
+for (let a = 0; a < gameOfLifeObjectArray.length; a ++) {
+    
+    let i = gameOfLifeObjectArray[a].location[0];
+    let j = gameOfLifeObjectArray[a].location[1];
 
+    findNeighbours.forEach(([x,y]) => {
+        const loc1 = i + x;
+        const loc2 = j + y;
+
+        if(gameOfLifeObjectArray[a].location[loc1,loc2] && gameOfLifeObjectArray[a].location[loc1,loc2]) {
+            if (gameOfLifeObjectArray[a].state === true) {
+                numberOfAliveNeighbours = numberOfAliveNeighbours + 1
+            };
+        };
+});
+};
 console.log(numberOfAliveNeighbours);
-
-for (let outerArray = 0; outerArray < gameOfLifeObjectArray.length; outerArray ++) {
-    for (let innerArray = 0; innerArray < gameOfLifeObjectArray[outerArray].length; innerArray ++) {
-        if 
-    };
-};
-
 //Create IF statements to handle each of the Game of Life rules
-
-
-
-
-
-
 
---
+++ gameOfLifeClasses.js
@@ -0,0 +1,7 @@
+class Cell {
+    constructor(row, column){
+        this.row = row;
+        this.column = column;
+        this.dead = false
+    }
+}
