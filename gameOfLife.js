5c0465f0fdcd60ff21818d170b37c1338018469d
Nicole <nicole.barnett.nz@gmail.com>
2021-04-17 17:34:36 +1200

    created function to calculate the number of alive cells in the entire array.

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,6 +1,6 @@
 //Create 2D array
 
-const gameOfLifeStructureArray = [[1,2,3],[4,5,6],[7,8,9]];
+const gameOfLifeStructureArray = [[1,2,3],[4,5,6],[7,8,9]]; //plan to expand this to a larger array structure once I have the functionality working
 const gameOfLifeObjectArray = [];
 
 function getRandomInt(max) {
@@ -8,7 +8,7 @@ function getRandomInt(max) {
 }
 
 //Create FOR loop, to loop through each element in the array 
-//Create a new object with each iteration which holds the cell location and randomly assigns dead or alive state.
+//Create a new object with each iteration, that holds the cell location and randomly assigns dead or alive state.
 
 for (let i = 0; i < gameOfLifeStructureArray.length; i ++) {
     for (let j = 0; j < gameOfLifeStructureArray[i].length; j ++) {
@@ -18,7 +18,23 @@ for (let i = 0; i < gameOfLifeStructureArray.length; i ++) {
         gameOfLifeObjectArray.push(cell);
     };
 };
-console.log(gameOfLifeObjectArray);
+console.log(gameOfLifeObjectArray[0].state);
+//Create function to count how many alive neighbours a cell has
+
+let numberOfAliveNeighbours = 0;
+
+for (let i = 0; i < gameOfLifeObjectArray.length; i ++) {
+    if (gameOfLifeObjectArray[i].state === 1) {
+        numberOfAliveNeighbours = numberOfAliveNeighbours + 1;
+    };
+};
+
+console.log(numberOfAliveNeighbours);
+
+//Create IF statements to handle each of the Game of Life rules
+
+
+
 
 
 
