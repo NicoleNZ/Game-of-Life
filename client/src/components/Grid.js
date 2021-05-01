78e365ecc66a2a2195ad82fc2199063beccdc190
Nicole <nicole.barnett.nz@gmail.com>
2021-05-01 13:15:34 +1200

    grid now rendering correctly

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -5,36 +5,92 @@ import { nextGeneration } from "./NextGeneration";
 
 export const Grid = () => {
   const [grid, setGrid] = useState([]);
+  const [gridHtml, setGridHtml] = useState([]);
+
+  const transFormGridIntoHtml = (currentGrid) => {
+    console.log('currentGrid',currentGrid);
+    let currentRow = 0;
+    let columns = [];
+    const createGridHtml = [];
+    for (let i = 0; i < currentGrid.length; i++) {
+        console.log('check this: ', currentGrid[i].location[0]);
+        console.log(currentGrid[i + 1]);
+      if (currentGrid[i].location[0] === currentRow && currentGrid[i].state === true) {
+          columns.push(<div className="liveCell" style={{ display: "inline-flex" }}>{currentGrid[i].location}</div>          )
+      };
+      
+      if (currentGrid[i].location[0] === currentRow && currentGrid[i].state === false) {
+        columns.push(
+              <div className="deadCell" style={{ display: "inline-flex" }}>{currentGrid[i].location}</div>
+          )
+      } 
+      else if (currentGrid[i].location[0] > currentRow && currentGrid[i + 1] !== undefined) {
+        createGridHtml.push(columns);
+        console.log(columns);
+          currentRow = currentGrid[i].location[0];
+          columns = [];
+          if (currentGrid[i].location[0] === currentRow && currentGrid[i].state === true) {
+            columns.push(<div className="liveCell" style={{ display: "inline-flex" }}>{currentGrid[i].location}</div>          )
+          };
+        
+          if (currentGrid[i].location[0] === currentRow && currentGrid[i].state === false) {
+            columns.push(
+                <div className="deadCell" style={{ display: "inline-flex" }}>{currentGrid[i].location}</div>
+            );
+          console.log("currentRow", currentRow);
+          }
+      }
+      else if (currentGrid[i + 1] === undefined) {
+        createGridHtml.push(columns);
+        console.log("undefined", columns);
+          currentRow = currentGrid[i].location[0];
+          columns = [];
+          if (currentGrid[i].location[0] === currentRow && currentGrid[i].state === true) {
+            columns.push(<div className="liveCell" style={{ display: "inline-flex" }}>{currentGrid[i].location}</div>          )
+          };
+        
+          if (currentGrid[i].location[0] === currentRow && currentGrid[i].state === false) {
+            columns.push(
+                <div className="deadCell" style={{ display: "inline-flex" }}>{currentGrid[i].location}</div>
+            );
+          }
+          console.log("currentRow", currentRow);
+      };
+  };
+  console.log("this is createGridHtml", createGridHtml);
+  setGridHtml(createGridHtml);
+  };
 
   useEffect(() => {
-    let currentRow = -1;
-    let columns = [];
-    const createGrid = [];
     const firstGen = FirstGeneration();
-    for (let i = 0; i < firstGen.length; i++) {
+    transFormGridIntoHtml(firstGen);
+    // let currentRow = -1;
+    // let columns = [];
+    // const createGrid = [];
+    // for (let i = 0; i < firstGen.length; i++) {
         
-        if (firstGen[i].location[0] === currentRow && firstGen[i].state === true) {
-            columns.push(
-                <div className="liveCell" style={{ display: "inline-flex" }}></div>
-            )
-        };
+    //     if (firstGen[i].location[0] === currentRow && firstGen[i].state === true) {
+    //         columns.push(
+    //             <div className="liveCell" style={{ display: "inline-flex" }}></div>
+    //         )
+    //     };
         
-        if (firstGen[i].location[0] === currentRow && firstGen[i].state === false) {
-            columns.push(
-                <div className="deadCell" style={{ display: "inline-flex" }}></div>
-            )
-        } else if (firstGen[i].location[0] > currentRow) {
-            createGrid.push(columns);
-            currentRow = firstGen[i].location[0];
-            columns = [];
-        };
-    };
-    console.log("this is createGrid", createGrid);
-    setGrid(createGrid);
-    console.log("this is grid:", grid);
+    //     if (firstGen[i].location[0] === currentRow && firstGen[i].state === false) {
+    //         columns.push(
+    //             <div className="deadCell" style={{ display: "inline-flex" }}></div>
+    //         )
+    //     } else if (firstGen[i].location[0] > currentRow) {
+    //         createGrid.push(columns);
+    //         currentRow = firstGen[i].location[0];
+    //         columns = [];
+    //     };
+    // };
+    // console.log("this is createGrid", createGrid);
+    // setGrid(createGrid);
+    // console.log("this is grid:", grid);
 }, []);
 
-  const setNextGeneration = () => {
+  const processNextGeneration = () => {
     const newGrid = nextGeneration(grid);
     setGrid(newGrid);
   };
@@ -43,13 +99,13 @@ export const Grid = () => {
     <Container className="container">
       <Row>
         <Col>
-          <Button onClick={setNextGeneration}>Button</Button>
+          <Button onClick={processNextGeneration}>Button</Button>
         </Col>
       </Row>
       <Row>
         <Col>
           <div className="grid">
-            {grid.map((el, i) => {
+            {gridHtml.map((el, i) => {
               return <span key={i}>{el}</span>;
             })}
           </div>
