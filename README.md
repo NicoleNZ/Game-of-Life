0a4df7d6f0bd71c16ed7e40b4e97c0a018311aa7
Nicole <nicole.barnett.nz@gmail.com>
2021-05-06 20:56:04 +1200

    updating README

--- README.md
+++ README.md
@@ -2,4 +2,34 @@
 
 https://github.com/AnthonyMastrean/code-retreat
 
+# Nicole's Game of Light - based on Conway's Game of Life
+
+This MERN app is a playable, galactic interpretation of Conway's Game of Life.  It also contains functionality to record your favourite galaxies!  Perfect for those budding astronomers out there. 
+
+## Installation
+
+Use npm install to install Nicole's Game of Light.
+
+```bash
+npm install 
+```
+
+## Run
+
+To run, split your terminal view and navigate one to Client, and the other to Server.  Once here, simply type the below command into each terminal:
+
+```bash
+npm start 
+```
+The app will start and automatically open in a browser.
+
+## Logging In
+
+Please create your own login details to access the game and create you first galaxy record.
+
+## Screenshots
+
+
+
+
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,68 @@
-import React from "react";
-import { Navigation } from "./components/Navigation";
-import { Grid } from "./components/game/Grid"
-import './App.css';
-import { GalaxyContainer } from "./components/galaxy/GalaxyContainer";
+// import React from "react";
+// import { Navigation } from "./components/Navigation";
+// import { Grid } from "./components/game/Grid"
+// import './App.css';
+// import { GalaxyContainer } from "./components/galaxy/GalaxyContainer";
 
 
-const App = () => {
+// const App = () => {
   
 
-  return (
-    <div>
-      <Navigation />
-      <div className="parent-container">
-          <Grid />
-      </div>
-      <GalaxyContainer />
-    </div>
-  );
-}
-
-export default App;
-
-// import React from 'react';
-
-// import {
-//   BrowserRouter,
-//   Switch,
-//   Route
-// } from 'react-router-dom';
-// import { Grid } from './components/game/Grid';
-// import { SignIn } from "./components/user/SignIn"; 
-// import { SignUp } from "./components/user/SignUp";
-
-// function App() {
 //   return (
-//     <BrowserRouter>
-//       <div className="App">
-//         <Switch>
-
-//           <Route exact path="/">
-//             <SignIn /> 
-//           </Route>
-//           <Route exact path="/signup">
-//             <SignUp /> 
-//           </Route>
-//           <Route exact path="/grid">
+//     <div>
+//       <Navigation />
+//       <div className="parent-container">
 //           <Grid />
-//           </Route>
-
-//         </Switch>
 //       </div>
-//     </BrowserRouter>
+//       <GalaxyContainer />
+//     </div>
 //   );
-// };
+// }
+
 // export default App;
+
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import "./App.css";
+import { Navigation } from "./components/Navigation";
+import { Grid } from './components/game/Grid';
+import { GalaxyContainer } from "./components/galaxy/GalaxyContainer";
+import { SignIn } from "./components/user/SignIn"; 
+import { SignUp } from "./components/user/SignUp";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+
+          <Route exact path="/">
+            <SignIn /> 
+          </Route>
+
+          <Route exact path="/signup">
+            <SignUp /> 
+          </Route>
+
+          <Route exact path="/grid">
+            <div>
+              <Navigation />
+              <div className="parent-container">
+                <Grid />
+              </div>
+            <GalaxyContainer />
+            </div>
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+export default App;
